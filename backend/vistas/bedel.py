cfa9193e3cec932f17e6a1ad87406db28fb83ff9
BerseKIEL <exebarco280320@gmail.com>
2022-07-07 02:50:01 -0300

    template dir Bedel

--- backend/vistas/bedel.py
+++ backend/vistas/bedel.py
@@ -3,6 +3,8 @@ from flask_login import login_required, current_user
 
 from ..models.entidad.EntidadUsuario import User
 
+import os
+
 from .. import mysql
 
 template_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..', 'frontend', 'templates','bedel'))
